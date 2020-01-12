1121424cf1243b6d8e247f71f358226c3e317d56
thelonius182 <lcavdakker@gmail.com>
2020-01-12 13:36:40 +0100

    more work, 2020-01-12, 13:36

--- src/main.R
+++ src/main.R
@@ -6,6 +6,11 @@ library(yaml)
 library(stringr)
 library(dplyr)
 library(lubridate)
+library(gmailr)
+library(purrr)
+
+gm_auth_configure(path = "cz-studiomails.json")
+gm_auth(email = "cz.teamservice@gmail.com")
 
 # define functions ----
 cz_extract_sheet <- function(ss_name, sheet_name) {
@@ -26,15 +31,14 @@ config <- read_yaml("config.yaml")
 
 # downloads GD ----
 
-# aanmelden bij GD loopt via de procedure die beschreven is in "Operation Missa > Voortgang > 
-# 4.Toegangsrechten GD".
+# aanmelden bij GD loopt via de procedure die beschreven is in 
+# "Operation Missa > Voortgang > 4.Toegangsrechten GD".
 
 # Roosters 3.0 ophalen bij GD
 path_roosters <- paste0(config$gs_downloads, "/", "roosters.xlsx")
 drive_download(file = cz_get_url("roosters"), overwrite = T, path = path_roosters)
 
 # sheets als df ----
-
 tbl_raw_presentatie <- cz_extract_sheet(path_roosters, sheet_name = "presentatie")
 
 # refactor raw tables 
@@ -43,19 +47,18 @@ tbl_presentatie.I <-  tbl_raw_presentatie %>%
   select(uitzending = B, uren, pres = Presentatie, tech = Techniek, live = Status) %>% 
   mutate(live = if_else(live == "Live", T, F))
 
-# teamleden: via SalesForce
+# teamleden: vooraf opgehaald uit SalesForce en beschikbaar in Downloads
 path_teamleden <- paste0(config$gs_downloads, "/studioteamleden.csv")
 tbl_teamleden <- read_csv(file = path_teamleden,
-                            col_types = cols(NPE01__ALTERNATEEMAIL__C = col_skip())
-  ) %>% 
+                          col_types = cols(NPE01__ALTERNATEEMAIL__C = col_skip())
+) %>% 
   mutate(tlf = if_else(!is.na(MOBILEPHONE), MOBILEPHONE, PHONE)) %>% 
   select(-MOBILEPHONE, -PHONE)
 
 tbl_presentatie <- tbl_presentatie.I %>% 
   left_join(tbl_teamleden, by = c("pres" = "NAME")) %>% 
   left_join(tbl_teamleden, by = c("tech" = "NAME")) %>% 
-  filter(live & (TEAMSERVICE_ONTVANGERS__C.x | TEAMSERVICE_ONTVANGERS__C.y)) %>% 
-  mutate(pres_roep = FIRSTNAME.x,
+  rename(pres_roep = FIRSTNAME.x,
          pres_mail = EMAIL.x,
          pres_tlf = tlf.x,
          pres_send = TEAMSERVICE_ONTVANGERS__C.x,
@@ -63,45 +66,164 @@ tbl_presentatie <- tbl_presentatie.I %>%
          tech_mail = EMAIL.y,
          tech_tlf = tlf.y,
          tech_send = TEAMSERVICE_ONTVANGERS__C.y
-  )
+  ) %>% 
+  filter(live & (pres_send | tech_send)) 
 
 rm(tbl_presentatie.I, tbl_raw_presentatie, tbl_teamleden)
 
-source("src/send_mail.R", encoding = "UTF-8")
-
-# overmorgen
+# overmorgen ----
 overmorgen <- today(tzone = "Europe/Amsterdam") + ddays(2L)
 
 mailen_dag2_pres <- tbl_presentatie %>% 
-  select(pres_mail, pres_roep, uitzending, pres_send, uren, tech, tech_tlf) %>% 
+  select(pres, pres_mail, pres_roep, uitzending, pres_send, uren, tech, tech_tlf) %>% 
   filter(uitzending == overmorgen & pres_send)
 
-t1 <- reminder_text(pres = mailen_dag2_pres$pres_roep,
-                    wanneer = "Overmorgen",
-                    tech = mailen_dag2_pres$tech,
-                    tlf = mailen_dag2_pres$tech_tlf,
-                    hoe_laat = mailen_dag2_pres$uren,
-                    uitz = format(mailen_dag2_pres$uitzending, "%A %d %B"))
+if (nrow(mailen_dag2_pres) > 0) {
+  mail_body_dag2_pres <- "Dag %s,
 
-send_reminder(pt_mail = "lcavdakker@gmail.com", reminder_txt = t1)
+Overmorgen, %s van %s, ben je weer in de ether. 
+Je technicus is dan %s (%s).
+
+Met de groeten van 
+CZ-teamservice
+" 
+
+  tbl_msg_data <- mailen_dag2_pres %>% 
+    mutate(
+      # To = sprintf("%s <%s>", pres, pres_mail),
+      To = sprintf("%s <lcavdakker@gmail.com>", pres),
+      From = "no_reply_cz.teamservice@gmail.com",
+      Subject = sprintf("Herinnering: %s", 
+                        str_replace(format(uitzending, "%A %d %B"), " 0", " ")),
+      body = sprintf(mail_body_dag2_pres,
+                     pres_roep,
+                     str_replace(format(uitzending, "%A %d %B"), " 0", " "),
+                     uren,
+                     tech,
+                     tech_tlf)) %>% 
+    select(To, From, Subject, body)
+  
+  write_csv(tbl_msg_data, "tbl-msg-data.csv")
+  
+  cz_msgs <- tbl_msg_data %>% pmap(gm_mime)
+  
+  safe_send_msg <- safely(gm_send_message)
+  sent_mail <- cz_msgs %>% map(safe_send_msg)
+}
 
 mailen_dag2_tech <- tbl_presentatie %>% 
-  select(tech_mail, tech_roep, uitzending, uren, pres, pres_tlf) %>% 
+  select(tech, tech_mail, tech_roep, uitzending, tech_send, uren, pres, pres_tlf) %>% 
   filter(uitzending == overmorgen & tech_send)
 
-saveRDS(mailen_dag2_tech, file = paste0(config$cz_rds_store, "mailen_dag2_tech.RDS"))
+if (nrow(mailen_dag2_tech) > 0) {
+  
+  mail_body_dag2_tech <- "Dag %s,
+  
+Overmorgen, %s van %s, ben je weer in de ether. 
+Je presentator is dan %s (%s).
+  
+Met de groeten van 
+CZ-teamservice
+" 
+  
+  tbl_msg_data <- mailen_dag2_tech %>% 
+    mutate(
+      # To = sprintf("%s <%s>", pres, pres_mail),
+      To = sprintf("%s <lcavdakker@gmail.com>", tech),
+      From = "no_reply_cz.teamservice@gmail.com",
+      Subject = sprintf("Herinnering: %s", 
+                        str_replace(format(uitzending, "%A %d %B"), " 0", " ")),
+      body = sprintf(mail_body_dag2_tech,
+                     tech_roep,
+                     str_replace(format(uitzending, "%A %d %B"), " 0", " "),
+                     uren,
+                     pres,
+                     pres_tlf)) %>% 
+    select(To, From, Subject, body)
+  
+  write_csv(tbl_msg_data, "tbl-msg-data.csv")
+  
+  cz_msgs <- tbl_msg_data %>% pmap(gm_mime)
+  
+  safe_send_msg <- safely(gm_send_message)
+  sent_mail <- cz_msgs %>% map(safe_send_msg)
+}
 
-# over 10 dagen
-over_10_dagen <- today(tzone = "Europe/Amsterdam") + ddays(10L)
+# over 10 dagen ----
+over_10_dagen <- today(tzone = "Europe/Amsterdam") + ddays(11L)
 
 mailen_dag10_pres <- tbl_presentatie %>% 
-  select(pres_mail, pres_roep, uitzending, uren, tech, tech_tlf) %>% 
+  select(pres, pres_mail, pres_roep, uitzending, pres_send, uren, tech, tech_tlf) %>% 
   filter(uitzending == over_10_dagen & pres_send)
 
-saveRDS(mailen_dag10_pres, file = paste0(config$cz_rds_store, "mailen_dag10_pres.RDS"))
+if (nrow(mailen_dag10_pres) > 0) {
+  mail_body_dag10_pres <- "Dag %s,
+  
+Over 10 dagen, op %s van %s, ben je weer in de ether. 
+Je technicus is dan %s (%s).
+  
+Met de groeten van 
+CZ-teamservice
+" 
+  
+  tbl_msg_data <- mailen_dag10_pres %>% 
+    mutate(
+      # To = sprintf("%s <%s>", pres, pres_mail),
+      To = sprintf("%s <lcavdakker@gmail.com>", pres),
+      From = "no_reply_cz.teamservice@gmail.com",
+      Subject = sprintf("Herinnering: %s", 
+                        str_replace(format(uitzending, "%A %d %B"), " 0", " ")),
+      body = sprintf(mail_body_dag10_pres,
+                     pres_roep,
+                     str_replace(format(uitzending, "%A %d %B"), " 0", " "),
+                     uren,
+                     tech,
+                     tech_tlf)) %>% 
+    select(To, From, Subject, body)
+  
+  write_csv(tbl_msg_data, "tbl-msg-data.csv")
+  
+  cz_msgs <- tbl_msg_data %>% pmap(gm_mime)
+  
+  safe_send_msg <- safely(gm_send_message)
+  sent_mail <- cz_msgs %>% map(safe_send_msg)
+}
 
 mailen_dag10_tech <- tbl_presentatie %>% 
-  select(tech_mail, tech_roep, uitzending, uren, pres, pres_tlf) %>% 
+  select(tech, tech_mail, tech_roep, uitzending, tech_send, uren, pres, pres_tlf) %>% 
   filter(uitzending == over_10_dagen & tech_send)
 
-saveRDS(mailen_dag10_tech, file = paste0(config$cz_rds_store, "mailen_dag10_tech.RDS"))
+if (nrow(mailen_dag10_tech) > 0) {
+  
+  mail_body_dag10_tech <- "Dag %s,
+  
+Over 10 dagen, op %s van %s, ben je weer in de ether. 
+Je presentator is dan %s (%s).
+  
+Met de groeten van 
+CZ-teamservice
+" 
+  
+  tbl_msg_data <- mailen_dag10_tech %>% 
+    mutate(
+      # To = sprintf("%s <%s>", pres, pres_mail),
+      To = sprintf("%s <lcavdakker@gmail.com>", tech),
+      From = "no_reply_cz.teamservice@gmail.com",
+      Subject = sprintf("Herinnering: %s", 
+                        str_replace(format(uitzending, "%A %d %B"), " 0", " ")),
+      body = sprintf(mail_body_dag10_tech,
+                     tech_roep,
+                     str_replace(format(uitzending, "%A %d %B"), " 0", " "),
+                     uren,
+                     pres,
+                     pres_tlf)) %>% 
+    select(To, From, Subject, body)
+  
+  write_csv(tbl_msg_data, "tbl-msg-data.csv")
+  
+  cz_msgs <- tbl_msg_data %>% pmap(gm_mime)
+  
+  safe_send_msg <- safely(gm_send_message)
+  sent_mail <- cz_msgs %>% map(safe_send_msg)
+}
+
